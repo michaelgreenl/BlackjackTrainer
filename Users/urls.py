adf512ce751c854a94a85eeacd9984878ca30668
michaelgreen <lgreener25@gmail.com>
2020-05-17 18:16:21 -0400

    Added account and login templates as well as more checks in the navbar template

--- Users/urls.py
+++ Users/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'Users'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('account/', views.account_view, name='account'),
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 def register_view(request):
-    return render(request, 'Users/register.html')
+    context = {
+        'signed_in': False,
+        'form': UserCreationForm,
+    }
+    return render(request, 'Users/register.html', context)
+
+
+def login_view(request):
+    context = {
+        'signed_in': False,
+    }
+    return render(request, 'Users/login.html', context)
+
+
+def account_view(request):
+    context = {
+        'signed_in': True,
+    }
+    return render(request, 'Users/account.html', context)
