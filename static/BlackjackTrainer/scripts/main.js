f557e18df5cce40be946f9b28935283fb0f8129d
michaelgreen <lgreener25@gmail.com>
2020-06-04 14:16:33 -0400

    Added a main.js for all event handlers.

---
+++ static/BlackjackTrainer/scripts/main.js
@@ -0,0 +1,32 @@
+let navAccountBtn = document.getElementById('nav-account-btn');
+let navDropdown = document.getElementById('navbar-dropdown');
+
+navDropdown.style.display = 'none';
+
+document.body.addEventListener('click', function () {
+    if (navDropdown.style.display === 'flex') {
+        navDropdown.style.display = 'none';
+    }
+}, false);
+
+navAccountBtn.addEventListener('click', function (ev) {
+    if (navDropdown.style.display === 'none') {
+        navDropdown.style.display = 'flex';
+    } else if (navDropdown.style.display === 'flex') {
+        navDropdown.style.display = 'none';
+    }
+    ev.stopPropagation();
+}, false);
+
+
+let alert = document.getElementById('alert');
+
+if (alert !== null) {
+    document.body.addEventListener('click', function () {
+        alert.style.display = 'none';
+    });
+    navAccountBtn.addEventListener('click', function () {
+        alert.style.display = 'none';
+    });
+}
+
